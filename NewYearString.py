a52bf9d5b4f9d6ac89edfe9c089d9ae0e82deff2
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-07 08:05:19 +0000

    Refactor NewYearString.py to implement solve_case function and remove printMinSteps

--- NewYearString.py
+++ NewYearString.py
@@ -1,45 +1,69 @@
-def printMinSteps(testcases):
-    for testcase in testcases:
-        last_year_present = False
-        new_year_present = False
-        last_year = []
-        
-        for char in testcase:
-            if char == "2":
-                if last_year:
-                    if last_year[-1] != "0":
-                        last_year = []
+from collections import defaultdict
 
-                last_year.append(char)
+PAT25 = "2025"
+PAT26 = "2026"
+CHARS = ['0', '2', '5', '6']
 
-            elif char == "0" and last_year and last_year[-1] == "2":
-                last_year.append(char)
+def advance(pattern, k, ch):
+    while k > 0 and pattern[k] != ch:
+        k -= 1
+    if pattern[k] == ch:
+        k += 1
+    return k
 
-            elif char == "5" and len(last_year) == 3 and last_year[-1] == "2":
-                last_year_present = True
-                last_year = []
+def solve_case(s):
+    n = len(s)
+    INF = 10**9
 
-            elif char == "6" and len(last_year) == 3 and last_year[-1] == "2":
-                new_year_present = True
-                break
+    dp = defaultdict(lambda: INF)
+    dp[(0, 0, 0, False, False)] = 0
 
-            else:
-                last_year = []
+    for pos in range(n):
+        ndp = defaultdict(lambda: INF)
+        for (i, a, b, f25, f26), cost in dp.items():
+            if i != pos:
+                continue
+            for ch in CHARS:
+                ncost = cost + (ch != s[pos])
 
-        if new_year_present:
-            print(0)
-        elif last_year_present:
-            print(1)
-        else:
-            print(0)
+                na = advance(PAT25, a, ch)
+                nb = advance(PAT26, b, ch)
 
-if "__main__":
-    n = int(input())
+                nf25 = f25 or (na == 4)
+                nf26 = f26 or (nb == 4)
+
+                if na == 4:
+                    na = 3
+                if nb == 4:
+                    nb = 3
+
+                state = (pos + 1, na, nb, nf25, nf26)
+                ndp[state] = min(ndp[state], ncost)
+        dp = ndp
+
+    ans = INF
+    for (pos, a, b, f25, f26), cost in dp.items():
+        if pos == n and (f26 or not f25):
+            ans = min(ans, cost)
+
+    return ans
+
+
+if __name__ == "__main__":
+    t = int(input())
     testcases = []
-    while n > 0:
-        chars_len = int(input())
-        chars = input()
-        testcases.append(chars)
-        n -= 1
 
-    printMinSteps(testcases)
+    # read all input first
+    for _ in range(t):
+        n = int(input())
+        s = input().strip()
+        testcases.append(s)
+
+    # process
+    results = []
+    for s in testcases:
+        results.append(solve_case(s))
+
+    # output
+    for r in results:
+        print(r)
