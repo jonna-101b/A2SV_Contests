41c236dee9690153af9f0c1555b546fd4e9b8b13
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-10 17:37:19 +0000

    Simplify logic in KidusScrambledBookII.py by removing trailing zero check and streamlining split number validation

--- KidusScrambledBookII.py
+++ KidusScrambledBookII.py
@@ -1,24 +1,10 @@
 tc = int(input())
 
-
-def check_trailing(num):
-    if num.startswith("0"):
-        return False
-    else:
-        return True
-
-
 for _ in range(tc):
     num = input()
     for i in range(len(num)):
-        num1 = num[: i + 1]
-        num2 = num[i + 1 :]
-        if num1 and num2 and check_trailing(num1) and check_trailing(num2):
-            num1 = int(num1)
-            num2 = int(num2)
-            if num2 > num1:
-                print(num1, num2)
-                break
-
+        if num[i] != 0 and int(num[i:]) > int(num[:i]):
+            print(num[:i], num[i:])
+            break
     else:
         print(-1)
