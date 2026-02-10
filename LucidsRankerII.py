1f48a70d0e712c288159d1607353f22ab8369080
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-10 18:40:37 +0000

    Comment out initial ranking logic in LucidsRankerII.py and retain final ranking calculation

--- LucidsRankerII.py
+++ LucidsRankerII.py
@@ -1,25 +1,33 @@
-if "__main__":
-    list_length = int(input())
-    numbers = list(map(int, input().split()))
+# if "__main__":
+#     list_length = int(input())
+#     numbers = list(map(int, input().split()))
 
-    no_copy = numbers.copy()
-    no_copy.sort(reverse = True)
-    ranks = dict()
-    last_num = None
-    frequency = 0
-    for num in no_copy:
-        if last_num is None:
-            ranks[num] = 1
-        elif num != last_num:
-            ranks[num] = ranks[last_num] + 1 + frequency
-            frequency = 0
-        else:
-            frequency += 1
+#     no_copy = numbers.copy()
+#     no_copy.sort(reverse = True)
+#     ranks = dict()
+#     last_num = None
+#     frequency = 0
+#     for num in no_copy:
+#         if last_num is None:
+#             ranks[num] = 1
+#         elif num != last_num:
+#             ranks[num] = ranks[last_num] + 1 + frequency
+#             frequency = 0
+#         else:
+#             frequency += 1
         
-        last_num = num
+#         last_num = num
 
-    res = []
-    for num in numbers:
-        res.append(ranks[num])
+#     res = []
+#     for num in numbers:
+#         res.append(ranks[num])
 
-    print(*res)
+#     print(*res)
+
+
+tc = int(input())
+ranks = []
+nums = list(map(int, input().split()))
+for i in range(len(nums)):
+    ranks.append(1 + len([x for x in nums if x > nums[i]]))
+print(*ranks)
