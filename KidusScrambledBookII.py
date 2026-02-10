f23189db69d4bd1b4b84a6384cfd442c2da8743e
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-10 17:40:38 +0000

    Refactor condition check in KidusScrambledBookII.py to improve clarity and ensure correct number comparisons

--- KidusScrambledBookII.py
+++ KidusScrambledBookII.py
@@ -3,8 +3,11 @@ tc = int(input())
 for _ in range(tc):
     num = input()
     for i in range(len(num)):
-        if num[i] != 0 and int(num[i:]) > int(num[:i]):
-            print(num[:i], num[i:])
-            break
+        if num[i] != "0":
+            n1 = num[i:]
+            n2 = num[:i]
+            if n1 and n2 and int(n1) > int(n2):
+                print(num[:i], num[i:])
+                break
     else:
         print(-1)
