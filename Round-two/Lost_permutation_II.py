8ef294a17ca272c70ec8472dde52da2fb85b282f
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-14 12:14:59 +0000

    Completed Lost permutation

---
+++ Round-two/Lost_permutation_II.py
@@ -0,0 +1,35 @@
+if __name__ == "__main__":
+    no_of_testcases = int(input())
+
+    for _ in range(no_of_testcases):
+        m, s = [int(num) for num in input().strip().split()]
+        b = [int(num) for num in input().strip().split()]
+        b.sort()
+        counter = 1
+        printed = False
+
+        for i in range(m):
+            if b[i] != counter:
+                s -= counter
+                counter += 1
+                if s < 0:
+                    print("NO")
+                    printed = True
+                    break
+
+            counter += 1
+
+        if not printed:
+            if s > 0:
+                while s > 0:
+                    if s >= counter:
+                        s -= counter
+                        counter += 1
+                    else:
+                        print("NO")
+                        printed = True
+                        break
+                
+                if not printed: print("YES")
+            else:
+                print("YES")
