d8b59972c53829c348348bb039c9b62f26463230
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-14 08:27:33 +0000

    Final 3

--- Round-two/NotePad.py
+++ Round-two/NotePad.py
@@ -1,19 +1,19 @@
 def solve():
-    t = int(input())
-    for _ in range(t):
-        n = int(input())
-        s = input().strip()
-        
-        seen = set()
-        possible = False
-        
-        for i in range(n - 1):
-            sub = s[i:i+2]
-            if sub in seen:
-                possible = True
-                break
-            seen.add(sub)
-        
-        print("YES" if possible else "NO")
+    n = int(input())
+    s = input().strip()
+    
+    seen = {}
+    for i in range(n - 1):
+        pair = s[i:i+2]
+        if pair in seen:
+            
+            if seen[pair] < i - 1:
+                print("YES")
+                return
+        else:
+            seen[pair] = i
+    print("NO")
 
-solve()
+t = int(input())
+for _ in range(t):
+    solve()
