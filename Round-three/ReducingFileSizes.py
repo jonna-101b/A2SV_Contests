141ecc996e3d9b530e0340d2abb5c89f17f91f70
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-21 07:46:29 +0000

    No change to be honest

--- Round-three/ReducingFileSizes.py
+++ Round-three/ReducingFileSizes.py
@@ -1,7 +1,29 @@
-files = []
-    total = 0
+if __name__ == "__main__":
+    n, m = [ int(num) for num in input().strip().split() ]
+    files = []
+    total_size = 0
 
     for _ in range(n):
         a, b = map(int, input().split())
-        total += a
-        files.append(a - b)  # saving
+        total_size += a
+        files.append(a - b)
+
+    if total_size <= m:
+        print(0)
+
+    else:
+        minimum = 0
+        files.sort(reverse = True)
+        printed = False
+
+        for diff in files:
+            minimum += 1
+            total_size -= diff
+
+            if total_size <= m:
+                print(minimum)
+                printed = True
+                break
+        
+        if not printed:
+            print(-1)
