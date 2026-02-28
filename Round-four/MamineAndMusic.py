4fb7f779ac3f8438bf0f83b8a491437aece50820
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-02-28 07:13:17 +0000

    Finished MamineAndMusic.py

---
+++ Round-four/MamineAndMusic.py
@@ -0,0 +1,20 @@
+if __name__ == "__main__":
+    n, k = [ int(num) for num in input().strip().split() ]
+    instruments = [ [i, int(num)] for i, num in enumerate(input().strip().split()) ]
+    instruments.sort(key = lambda x: x[1])
+    days = 0
+    counter = 0
+    indicies = []
+
+    for i, instrument in instruments:
+        days += instrument
+
+        if days > k:
+            break
+
+        counter += 1
+        indicies.append(i)
+
+    if counter:
+        print(counter)
+        print(*indicies)
