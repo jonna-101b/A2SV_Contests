60cd5313e0e97b2e7a3efe61f6574993b999ff0f
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-03-14 06:43:58 +0000

    Subtle change

--- Round-six/JuiceBox.py
+++ Round-six/JuiceBox.py
@@ -1,21 +1,17 @@
 from collections import defaultdict
 
 if __name__ == "__main__":
-    no_of_testcases = int(input())
+    t = int(input())
 
-    for _ in range(no_of_testcases):
-        n, k = [ int(num) for num in input().strip().split() ]
+    for _ in range(t):
+        n, k = map(int, input().split())
+
         cost_dict = defaultdict(int)
 
         for _ in range(k):
-            brand, cost = [ int(num) for num in input().strip().split() ]
-            cost_dict[brand] += cost
+            b, c = map(int, input().split())
+            cost_dict[b] += c
 
-        cost_list = [ (brand, cost_dict[brand]) for brand in cost_dict ]
-        cost_list.sort(reverse = True, key = lambda x: x[1])
-        max_cost = 0
+        costs = sorted(cost_dict.values(), reverse=True)
 
-        for i in range(min(n, len(cost_list))):
-            max_cost += cost_list[i][1]
-
-        print(max_cost)
+        print(sum(costs[:n]))
