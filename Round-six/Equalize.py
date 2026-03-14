3da6724996fc8239ac5204a01c1e5d9529f5dcf3
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-03-14 08:18:41 +0000

    Subtle change on Equalize.py

--- Round-six/Equalize.py
+++ Round-six/Equalize.py
@@ -1,15 +1,22 @@
-t = int(input())
+from collections import defaultdict
 
-for _ in range(t):
-    n = int(input())
-    a = list(map(int, input().split()))
-    
-    freq = {}
-    ans = 0
-    
-    for i in range(n):
-        key = a[i] - (i + 1)
-        freq[key] = freq.get(key, 0) + 1
-        ans = max(ans, freq[key])
-        
-    print(ans)
+if __name__ == "__main__":
+    no_of_testcases = int(input())
+
+    for _ in range(no_of_testcases):
+        n = int(input())
+        nums = [ int(num) for num in input().strip().split() ]
+        nums.sort()
+        diff = defaultdict(int)
+
+        for i in range(len(nums) - 1):
+            difference = nums[i+1] - nums[i]
+            if difference:
+                diff[difference] += 1
+
+        max_nums = 0
+        for difference in diff:
+            actual = min(n//difference, diff[difference] + 1)
+            max_nums = max(max_nums, actual)
+
+        print(max_nums)
