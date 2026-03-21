6463e2a16d76914e6814f363114b7ca7ce09b914
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-03-21 08:02:48 +0000

    Finished GoldRush

---
+++ Round-seven/GoldRush.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**7)
+
+if __name__ == "__main__":
+    no_of_testcases = int(input())
+
+    for _ in range(no_of_testcases):
+        n, m = [ int(num) for num in input().strip().split() ]
+
+        def check(n = n, m = m):
+            if n % 3 != 0 or m > n:
+                return False
+            
+            one = n // 3
+            two = one * 2
+            if one == m or two == m:
+                return True
+            
+            return check(one) or check(two)
+        
+        print("YES" if check() else "NO")
