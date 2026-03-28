a53aa3ac03cd4530e4ca2cd149021feef951d847
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-03-28 11:49:14 +0000

    Finished LOLLovers.py

---
+++ Round-eight/LOLLovers.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+if __name__ == "__main__":
+    n = int(input().strip())
+    foods = input().strip()
+    occurrence = Counter(foods)
+    me = { "L": 0, "O": 0}
+    friend = { "L": occurrence["L"], "O": occurrence["O"]}
+    counter = 1
+    found = False
+
+    for food in foods:
+        me[food] += 1
+        friend[food] -= 1
+
+        if me["L"] != friend["L"] and me["L"] != friend["L"] and (friend["L"] or friend["O"]):
+            found = True
+            break
+
+        counter += 1
+
+    if found:
+        print(counter)
+    else:
+        print(-1)
