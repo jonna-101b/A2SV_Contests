b59a7c2c96b14caf4ff8c306e2e00ef2d182290a
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-04-18 07:54:47 +0000

    Some change

--- Round-eleven/SequenceK-Labeling.py
+++ Round-eleven/SequenceK-Labeling.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     seq = list(map(int, input().split()))
     occurrence = Counter(seq)
     res = []
+    max_occurrence = occurrence[seq[0]]
 
     for num in seq:
         if occurrence[num] > k:
@@ -12,7 +13,16 @@ if __name__ == "__main__":
             break
 
         res.append(occurrence[num])
+        max_occurrence = max(max_occurrence, occurrence[num])
         occurrence[num] -= 1
 
-    print("YES" if res else "NO")
-    print(*res)
+    if res:
+        i = 0
+        for label in range(max_occurrence + 1, k + 1):
+            res[i] = label
+            i += 1
+            
+        print("YES")
+        print(*res)
+    else:
+        print("NO")
