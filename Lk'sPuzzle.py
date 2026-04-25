6d3f994da4319a05eb0a8929218421999f23300d
Jonna <151217814+jonna-101b@users.noreply.github.com>
2026-04-25 11:10:46 +0300

    Refactor DFS function and visited tracking

--- Lk'sPuzzle.py
+++ Lk'sPuzzle.py
@@ -1,42 +1,37 @@
 def solve(n, m, board):
-    found = False
+    visted = [[False]*m for _ in range(n)]
     
-    def DFS(row, col, color):
-        nonlocal found
-        
-        if row < 0 or row >= n:
-            return 
-        
-        if col < 0 or col >= m:
-            return
-        
-        if color is not None and board[row][col] != color:
-            return
-        
-        if board[row][col] == "VISiTED":
-            found = True
-            return
+    def DFS(r, c, pr, pc, color):
+        if visted[r][c]:
+            return True
             
-        DFS(row+1, col)
-        DFS(row-1, col)
-        DFS(row, col+1)
-        DFS(row, col-1)
-        board[row][col] = "VISiTED"
-        return
-
+        visted[r][c] = True
+        dxn = [(1, 0), (-1, 0), (0, 1), (0 ,-1)]
+        
+        for dr, dc in directions:
+            nr, nc = r + dr, c + dc
+            
+            if 0 <= nr < n and 0 <= nc < m:
+                if board[nr][nc] != color:
+                    continue
+                
+            if nr == pr and nc == pc:
+                continue
+            
+            if DFS(nr, nc, r, c, color):
+                return True
+                
+        return False
+ 
     for i in range(n):
         for j in range(m):
-            if board[i][j] != "VISiTED":
-                DFS(i, j)
-                
-            if found:
-                return "YES"
+            if not visted[i][j]:
+                if DFS(i, j, -1, -1, board[i][j]):
+                    return "YES"
                 
     return "NO"
-
+ 
 if __name__ == "__main__":
     n, m = list(map(int, input().split()))
-    board = [ board.append(input().split()) for _ in range(n) ]
+    board = [ input().strip().split() for _ in range(n) ]
     print(solve(n, m, board))
-    
-    
